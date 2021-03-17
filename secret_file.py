3914a20134373bbc177a7ae6d1fc2969375226fe
Enzo Mathe <enz.omathe@gmail.com>
2021-03-17 12:11:10 +0100

    new secret

--- secret_file.py
+++ secret_file.py
@@ -1,1 +1,3 @@
 var mongo_uri = "mongodb+srv://testuser:enz24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
+
+var mongo_uri = "mongodb+srv://testuser:emz24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
