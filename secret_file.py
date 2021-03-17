d78160b54f1d2a737cd48b818f8283692ed07e0c
Enzo Mathe <enz.omathe@gmail.com>
2021-03-17 12:04:47 +0100

    new git email address

--- secret_file.py
+++ secret_file.py
@@ -1,8 +1,1 @@
-var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
-
-var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
-
-
-var mongo_uri = "mongodb+srv://testuser:enz24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
-
 var mongo_uri = "mongodb+srv://testuser:enz24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
