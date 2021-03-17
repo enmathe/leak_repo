d0ea4184cc43b25a5659ec55316bcf34017689e8
Enzo Mathe <enzomathe+CoreAlerting+test@gmail.com>
2021-03-17 11:48:35 +0100

    secret not cached leak

--- secret_file.py
+++ secret_file.py
@@ -1,3 +1,6 @@
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
 
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
+
+
+var mongo_uri = "mongodb+srv://testuser:enz24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
