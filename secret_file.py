96cf99c3c1595644f670be14f00e1f2d7e6e074e
Enzo Mathe <enzomathe+test@gmail.com>
2021-03-17 11:58:33 +0100

    new email with secret leak

--- secret_file.py
+++ secret_file.py
@@ -4,3 +4,5 @@ var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.ne
 
 
 var mongo_uri = "mongodb+srv://testuser:enz24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
+
+var mongo_uri = "mongodb+srv://testuser:enz24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
