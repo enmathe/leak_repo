d758a1cc5ffd0f19b07fdb4a186d71d8e0b2e5e3
Enzo Mathe <enz.omathe@gmail.com>
2021-04-26 18:10:12 +0200

    remove key

--- secret_file.py
+++
@@ -1,5 +0,0 @@
-var mongo_uri = "mongodb+srv://testuser:enz24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
-
-var mongo_uri = "mongodb+srv://testuser:emz24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
-
-var mongo_uri = "mongodb+srv://testuser:mat24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
